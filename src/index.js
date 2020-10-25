6f6a98487d220a6008183136560acf0cbe89264d
Svetlana Borovitskaia <borovitzki@gmail.com>
2020-10-25 16:57:43 +0300

    feat: implement sorting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = (matrix) => {
+    if (!matrix || matrix.length === 0) {
+        return [];
+    } else {
+        let result = [];
+        let resultLength = 0;
+        for (i = 0; i < matrix.length; i++) {
+            resultLength += matrix[i].length;
+        }
+        let min = matrix[0][0];
+        for (i = 0; i < matrix.length; i++) {
+            if ((i % 2) == 0) {
+                Array.prototype.push.apply(result, matrix[i]);
+            } else {
+                Array.prototype.push.apply(result, matrix[i].reverse());
+            }
+        }
+        return result;
+    }
+};
